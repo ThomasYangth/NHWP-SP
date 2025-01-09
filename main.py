ac76a56388616f766b0e809a8ae5beb5498b6d4f
ThomasYangth <thomasyangth@outlook.com>
2025-01-09 16:50:27 +0800

    updated main

--- main.py
+++ main.py
@@ -2,13 +2,7 @@ from examples.compare_sp import *
 from os import chdir
 
 chdir("Datas/1D")
-
-#runExoticModel()
-#run2Dtslope()
-#run2DexpoC()
-#run2DexpoD()
-
 run1DModels()
-#YaoWangSpecs()
-#run2DModels()
-#plotvec()
+
+chdir("../2D")
+run2DModels()
